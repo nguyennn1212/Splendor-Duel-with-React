2b7858d2a15ec81361b1c8ce64334b3383403d36
nguyennn1212 <nguyengoc121203@gmail.com>
2026-04-07 19:03:55 -0400

    publish branch

--- src/game/gameStates.js
+++ src/game/gameStates.js
@@ -1,3 +1,9 @@
 import {GAME_CONFIG, GEM_COLORS, GAME_PHASES, PLAYER_TYPES} from '../data/constants'
 import {LEVEL_1_CARDS, LEVEL_2_CARDS, LEVEL_3_CARDS} from '../data/cards'
 import {ALL_NOBLES} from '../data/nobles'
+
+/** Create and initial game states when starting a new game */
+
+export function createInitialGameState() {
+    
+}
