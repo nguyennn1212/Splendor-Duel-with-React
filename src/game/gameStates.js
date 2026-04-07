99056fcfcaa6b07f2a6a03e0c3cd62b5011c237a
nguyennn1212 <nguyengoc121203@gmail.com>
2026-04-07 19:00:32 -0400

    added shuffle functions to cards constants to shuffle cards for a game

---
+++ src/game/gameStates.js
@@ -0,0 +1,3 @@
+import {GAME_CONFIG, GEM_COLORS, GAME_PHASES, PLAYER_TYPES} from '../data/constants'
+import {LEVEL_1_CARDS, LEVEL_2_CARDS, LEVEL_3_CARDS} from '../data/cards'
+import {ALL_NOBLES} from '../data/nobles'
