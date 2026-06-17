a021902580354a6ed6b4b14390d91be4011c2ff8
nguyennn1212 <nguyengoc121203@gmail.com>
2026-06-17 17:46:42 -0400

    Added a simple function to keep player's gem count

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import './App.css'
-import { useState } from 'react';
-import { generateGemGrid } from './game/gameStates';
+import "./App.css";
+import { useState } from "react";
+import { generateGemGrid } from "./game/gameStates";
+import { GEM_COLORS } from "./data/constants";
+import { playerInventory } from "./game/gemActions";
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
     <button
       className="square"
-      style={{ backgroundColor: value || 'lightgray', color: value === 'white' ? 'black' : 'white' }}
+      onClick={onSquareClick}
+      style={{
+        backgroundColor: value || "lightgray",
+        color: value === "white" ? "black" : "white",
+      }}
     >
-      {value || ''}
+      {value || ""}
     </button>
   );
 }
 
 function App() {
   const [gemGrid] = useState(() => generateGemGrid());
+  const [inventory, setInventory] = useState([0, 0, 0, 0, 0, 0, 0]);
+
+  const handleGemClicked = (color) => {
+    const nextInventory = playerInventory(inventory, color);
+    setInventory(nextInventory);
+  };
 
   return (
     <div>
       <h1>Splendor Duel</h1>
       <h2>Gem Grid Test</h2>
+      <div className="gem-board">
+        {gemGrid.map((row, rowIndex) => (
+          <div className="board-row" key={rowIndex}>
+            {row.map((gem, colIndex) => (
+              <Square
+                key={colIndex}
+                value={gem}
+                onSquareClick={() => handleGemClicked(gem)}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
 
-      {gemGrid.map((row, rowIndex) => (
-        <div className='board-row' key={rowIndex}>
-          {row.map((gem, colIndex) => (
-            <Square key={colIndex} value={gem} />
-          ))}
-        </div>
-      ))}
+      <div className="player-inventory-display">
+        <h3>Your current inventory</h3>
+        <p>[{inventory.join(", ")} ]</p>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/game/gemActions.js
+++ src/game/gemActions.js
@@ -1,3 +1,28 @@
-export function takeGemFromBoard(gameState, row, col) {
-    
-}
+import { GEM_COLORS } from "../data/constants";
+export const GEM_ORDER = [
+  "blue",
+  "white",
+  "green",
+  "black",
+  "red",
+  "gold",
+  "pearl",
+];
+
+export const playerInventory = (currentInventory, clickedColor) => {
+  if (clickedColor === GEM_COLORS.NONE) {
+    return currentInventory;
+  }
+
+  const gemIndex = GEM_ORDER.indexOf(clickedColor);
+  if (gemIndex === -1) {
+    return currentInventory;
+  }
+
+  const updatedInventory = [...currentInventory];
+  updatedInventory[gemIndex] += 1;
+
+  return updatedInventory;
+};
+
+//write a function to check for the amount of gems to be no more than 10
